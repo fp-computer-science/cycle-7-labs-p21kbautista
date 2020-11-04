99650fe923a4f3dd4410430111fd658891d4b581
Kyle Bautista <p21kbautista@fairfieldprep.org>
2020-11-04 09:20:55 -0500

    Add lab_2-1

---
+++ lab_2-1.py
@@ -0,0 +1,13 @@
+# Kyle Bautista (AMDG) 11/4/20
+
+print("Fairfield" + "Prep")
+
+print("-" * 20)
+
+print("I'm loving this story I'm reading, \"The Fall of the House of Usher\"")
+
+previous_string = "I'm loving this short story I'm reading, \"The Fall of the House of Usher\""
+print(len(previous_string))
+
+my_string = "apple"
+print(my_string[3])
