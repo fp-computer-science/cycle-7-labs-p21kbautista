90148fdb8de478711dffd5c58fe0bffe50ec09ff
Kyle <p21kbautista@fairfieldprep.org>
2020-11-05 11:40:36 -0500

    Add lab_3-1

---
+++ lab_3-1.py
@@ -0,0 +1,10 @@
+# Kyle Bautista (AMDG) 11/5/20
+
+school = "Fairfield Prep"
+
+first_half = school[:9]
+second_half = school[9:]
+
+print(first_half)
+print(second_half)
+print(first_half + second_half)
