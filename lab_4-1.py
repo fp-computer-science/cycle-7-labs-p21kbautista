017d85d57a5c91064d8774e41d4ad659b3bbc46d
Kyle Bautista <p21kbautista@fairfieldprep.org>
2020-11-06 13:21:01 -0500

    Add lab_4-1 and lab_4-2

---
+++ lab_4-1.py
@@ -0,0 +1,7 @@
+# Kyle Bautista (AMDG) 11/6/2020
+
+fstring = "flibbertigibett"
+print(fstring.find('t'))
+
+sstring = "I wish, I wish, I was a fish."
+print(sstring.split(','))
---
+++ lab_4-2.py
@@ -0,0 +1,6 @@
+# Kyle Bautista (AMDG) 11/6/2020
+
+name1 = input("Enter your name: ")
+name2 = input("Enter the other person's name: ")
+
+print("Hello, {0}. My name is {1}.".format(name1, name2))
